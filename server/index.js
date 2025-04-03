f5eb0274337503b276b385dd5f998d9b67ed8388
Fahad Saleem <fa7781824@gmail.com>
2025-04-03 10:28:15 -0700

    mern-app

---
+++ server/index.js
@@ -0,0 +1,35 @@
+require("dotenv").config();
+let express = require("express");
+let mongoose = require("mongoose");
+let cors = require("cors");
+const enquiryRouter = require("./App/routes/web/enquiryRoutes");
+
+let app = express();
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use("/api/website/enquiry", enquiryRouter);
+app.get('/',(req,res)=>{
+  res.send({
+    activeStatus:true,
+    error:false,
+  })
+})
+
+
+
+mongoose
+  .connect(process.env.DBURL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(" Connected to MongoDB Atlas");
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server is running on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((err) => {
+    console.error(" MongoDB Connection Error:", err);
+  });
